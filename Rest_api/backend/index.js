140a42e45a8edda5e72230ac306f9440ee27dbc7
Rishab0323 <srishbh574@gmail.com>
2025-07-10 12:13:00 +0530

    all rest api practise

--- Rest_api/backend/index.js
+++ Rest_api/backend/index.js
@@ -1,21 +1,80 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.urlencoded({extended:true}));
+const port = 3000;
+
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-let port = 3000;
+let posts = [
+    {
+        id : "1a",
+        username : "Rishab",
+        content : "Rest"
+    },
+    {
+        id : "2b",
+        username : "Restful",
+        content : "API"
+    },
+    {
+        id : "3c",
+        username : "Raju",
+        content : "apply for internship"
+    }
+]
 
-app.get("/register",(req,res) => {
-    let {user,password} = req.query;
-    res.send(`this is GET request,Welcome ${user}`);
-})
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
-app.post("/register",(req,res) =>{
-    let{user,passwaord}=req.body;
-    res.send("POST method is working");
+app.use(express.static(path.join(__dirname,"publix")));
+
+app.get("/posts",(req,res) =>{
+    res.render("index",{posts});
 });
 
-app.listen(port,(req,res) =>{
+app.get("/posts/new",(req,res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req,res) => {
+   
+    let {id,username,content} = req.body;
+    // res.send("Your post id added");
+    posts.push({id,username,content});
+    res.redirect("/posts");
+});
+
+
+app.get("/posts/:id", (req,res) => {
+   
+    let {id} = req.params;
+    // posts.push({id,username,content});
+    console.log(id);
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    // res.send("which id is yours");
+    // res.redirect("/posts");
+
+    res.render("show.ejs",{post});
+});
+
+//patch request
+app.patch("/posts/:id",(req,res) => {
+    let { id } = req.params;
+    let newContent =   req.body.content;
+    console.log(id);
+    res.send("patch request is working");
+});
+
+//edit request
+app.get("/posts/:id/edit",(req,res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs");
+});
+
+app.listen(port,() => {
     console.log(`listening to port ${port}`);
 });
