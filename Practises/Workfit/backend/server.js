2461bdaf8646954e4c9dacb924e6becd68e077ef
Rishab0323 <srishbh574@gmail.com>
2025-08-07 16:20:45 +0530

    define DB schema

--- Practises/Workfit/backend/server.js
+++ Practises/Workfit/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 app.use((req,res,next) => {
@@ -8,12 +9,27 @@ app.use((req,res,next) => {
     next();
 })
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log("connnected to db listening to port ", process.env.PORT);
+    });
+})
+.catch((error) => {
+    console.log(error)
+})
 
-app.get("/", (req,res) => {
+
+app.post("/", (req,res) => {
     res.json({msg : "welcome to the app"});
 });
 
-app.listen(process.env.PORT, () => {
-    console.log("listening to port ", process.env.PORT);
+app.delete("/", (req,res) => {
+    res.json({msg : "welcome to the app"});
 });
+
+app.patch("/", (req,res) => {
+    res.json({msg : "welcome to the app"});
+});
+
 
