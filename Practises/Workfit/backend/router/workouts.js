9ebd44e411f2d44fb246e36d5bdb9938080fbfb8
Rishab0323 <srishbh574@gmail.com>
2025-08-13 11:38:32 +0530

    post request done with testing

--- Practises/Workfit/backend/router/workouts.js
+++ Practises/Workfit/backend/router/workouts.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const Workout = require("../Models/workoutModel");
 
-const router = express.router();
+const router = express.Router();
+router.use(express.json());
 
 //to get all workouts
-router.get("/",() => {
-    res.json()
+router.get("/",(req,res) => {
+    res.json({msg : " hello route"});
 })
 
 //to get a single workout
@@ -13,8 +15,15 @@ router.get("/:id",(req,res) => {
 })
 
 //to post a new request
-router.post("/",(req,res) => {
-    res.json({msgg: "post a request"});
+router.post("/", async (req,res) => {
+    const {title,load,reps} = req.body;
+    try{
+        const workout = await Workout.create({title,load,reps});
+        res.status(200).json({workout});
+    }catch(error){
+        res.status(404).json({error : error.message}) //error with message
+    }
+    
 })
 
 //to delete a workout
