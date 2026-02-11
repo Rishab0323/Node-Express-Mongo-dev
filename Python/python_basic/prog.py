165c3af36c3d8118a458642388b11b2f436ae6cc
Rishab0323 <srishbh574@gmail.com>
2026-02-11 16:13:06 +0530

    python revision

---
+++ Python/python_basic/prog.py
@@ -0,0 +1,13 @@
+# number in python 
+x = 2  #int
+a = 2.3333 #float
+# b =  2 + 4i  complex num in py
+
+print( "integer no" ,x)
+
+#escape sequence 
+print('what is \'s is you name')
+
+#multple assignment
+a,b,c = 1,3.12,120
+print(a,b,c)
