5c406c4aa880a32b650eeaf188bd5c50d34ebd2a
Rishab0323 <srishbh574@gmail.com>
2026-02-12 16:25:35 +0530

    basic py

--- Python/python_basic/prog2.py
+++ Python/python_basic/prog2.py
@@ -1,5 +1,5 @@
 #loop 
-num = int(input("enter any number "))
+# # num = int(input("enter any number "))
 while (num < 10):
     print(num)
     num = num + 1
@@ -8,8 +8,35 @@ print("num val is ",num)
 
 #for loop
 arr = [1,2,4,5,7,8]
-# for i in arr:
-#     print(i)
+for i in arr:
+    print(i)
 
-for j in range(1,6,2):
-    print(j)
+for i in range(1,6):
+    
+    for j in range(i):
+        print('*',end=' ')
+    print()
+
+for i in range(1,6):
+    
+    for j in range(1,i):
+        print(j,end=' ')   
+    print()
+    if i == 3:
+            break
+
+def sum(int_x,float_y):
+    print('the sum of int & float value',int_x + float_y )
+
+sum(float_y=3,int_x=2)  #can use argument name to assign value 
+
+import re
+
+str1 = "she sell friuts"
+pat = "sell"
+if re.match(pat,str1):
+    print("string is found & is",pat)
+print("becuse match fun find only at beginning ")
+
+if re.search(pat,str1):
+    print("search fun is similar ")
